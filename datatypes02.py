ce33c61ba16301b050d8dbc33831dbe5f0fbc001
Tanayyy <pandeytanay926@gmail.com>
2026-05-29 23:33:10 +0530

    Add files via upload

---
+++ datatypes02.py
@@ -0,0 +1,16 @@
+age = 19 
+print(age)
+print(type(age))
+
+cgpa = 9.5
+print(cgpa)
+print(type(cgpa))
+
+name = "tanay"
+print(name)
+print(type(name))
+
+is_completed = True 
+print(is_completed)
+print(type(is_completed))
+
