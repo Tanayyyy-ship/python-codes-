fc43823c83d0c03ea07cc6c9f55e021b6f8cdcd8
Tanayyy <pandeytanay926@gmail.com>
2026-06-01 23:13:50 +0530

    Add files via upload

---
+++ typecasting03.py
@@ -0,0 +1,17 @@
+a = 19
+b = "19"
+d = 143
+print(a)
+print(type(a))
+
+
+print(b)
+print(type(b))
+#convert b to an integer 
+c = int(b)
+print(c)
+print(type(c))
+
+e = str(d)
+print(e)
+print(type(e))
