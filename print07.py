9cc91d555ee36c446398535287419c3dc28aa121
Tanayyy <pandeytanay926@gmail.com>
2026-06-03 22:55:15 +0530

    Add files via upload

---
+++ print07.py
@@ -0,0 +1,16 @@
+#print("hello wordl ", 'Tanay', )
+
+
+#print('hello world' ,  "Tanay" , 19 , sep=" ")
+
+
+
+#print('hello world' ,  "Tanay" , 19 , sep=" / ") 
+
+
+print('hello world' ,  "Tanay" , 19 , end="/ ")
+
+
+
+
+print('Tanay', 'byee', end=".")
