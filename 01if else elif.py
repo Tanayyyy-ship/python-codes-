7c74bc1d726a9b9decb95cc8d8448d6677a3e3b9
Tanayyy <pandeytanay926@gmail.com>
2026-06-05 23:10:44 +0530

    Add files via upload

---
+++ 01if else elif.py
@@ -0,0 +1,8 @@
+age = int(input("enter your age :"))
+
+if age < 18:
+    print("You are a minor.")
+elif age == 18:
+    print("You just became an adult!")
+else:
+    print("You are an adult.")
---
+++ 05break_continue_pass.py
@@ -0,0 +1,22 @@
+'''
+for i in range(0,21) :
+    print(i)             # break statement 
+    if i == 11 :
+        break  # cancel the execution of this loop
+'''
+'''
+# continue statement 
+for i in range (1 , 20) :
+     if i == 10 :
+          continue 
+         
+     print(i)
+     '''
+
+#pass statement 
+i = 3
+if i == 3 :
+    pass
+
+
+print("end of program ")
