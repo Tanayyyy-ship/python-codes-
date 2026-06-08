f674d25844b8463d42edc95f7508321219cc90b1
Tanayyy <pandeytanay926@gmail.com>
2026-06-08 23:24:06 +0530

    Add files via upload

---
+++ po2.py
@@ -0,0 +1,45 @@
+#q1
+'''
+
+a = int(input("enter a no :"))
+
+if(a>0):
+    print("positive")
+
+
+elif (a<0):
+     print("no is negative")
+
+
+else :
+     print("zero")
+'''
+
+
+
+
+#q2
+'''
+age = int(input("enter your age : "))
+
+if(age>=18):
+    print("you can vote bjp")
+
+else :
+    print("bhad me jao ")
+
+'''
+
+
+#q3
+
+
+num = int (input("enter a no :"))
+
+if(num>0):
+    print("even")
+
+else :
+    print("odd")
+
+    
