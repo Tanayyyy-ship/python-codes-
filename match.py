798d7b1e2ad49787ccf71825dcf1021946e9f368
Tanayyy <pandeytanay926@gmail.com>
2026-06-08 23:24:26 +0530

    Add files via upload

---
+++ match.py
@@ -0,0 +1,41 @@
+'''
+day = int(input("enter the no of days"))
+
+match day:
+    case 1:
+        print("sunday")
+    case 2:
+        print("monday")
+    case 3:
+        print("tuesday")
+    case 4:
+        print("wednesday")
+    case 5:
+        print("thuresday")
+    case 6:
+        print("friday")
+    case 7:
+        print("saturday")
+'''
+
+
+#q2
+
+'''
+num = int(input("enter a num:"))
+nom= int(input("enter the no:"))
+
+operation = input("choose operation")
+
+
+match operation :
+    case "+" :
+        print(num + nom)
+
+    case "-" :
+        print(num - nom)
+'''
+#q3
+
+for i in range(1 , 11):
+    print(i)
