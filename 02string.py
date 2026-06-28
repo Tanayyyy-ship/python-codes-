33ad05678227637df50928b9f2fbb88601e18da8
Tanayyy <pandeytanay926@gmail.com>
2026-06-28 23:14:13 +0530

    Add files via upload

---
+++ 02string.py
@@ -0,0 +1,10 @@
+name = "Tanay pandey"
+#print(name[0:2]) #goes from 0 to 2-1 
+
+#print(name[2:-1])# it will print n to e because it is n-1
+
+#print(name[0:10:2]) # it will remove one character because it is like n-1
+
+
+print(name[:4]) # replace the first empty no with 0
+print(name[1:]) # replace the second empty no with len
