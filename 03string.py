d233d52446b1fbf5585ca1b9c356fa4849f61e14
Tanayyy <pandeytanay926@gmail.com>
2026-07-03 22:25:51 +0530

    Add files via upload

---
+++ 03string.py
@@ -0,0 +1,35 @@
+#s = " hello world " # Strings are immutable
+
+#a = len(s)
+#print(a)
+#print(s.upper())
+#print(s.lower())
+#print(s.capitalize())
+#print(s.title())
+
+#print(s.rstrip())
+#print(s.strip())
+#print(s.lstrip())
+
+#text = "Python is fun"
+#print(text.find("is"))
+#print(text.replace("fun", "awesome"))
+
+text = "apple,banana,orange"
+fruits = text.split(",")
+print(fruits)  # Output: ['apple', 'banana', 'orange']
+
+new_text = " - ".join(fruits)
+print(new_text)  # Output: "apple - banana - orange"
+
+                  
+
+
+
+
+
+
+
+  
+
+
